5236727e3533f105d9aac313ec8d40f62975d246
Paul Williams <williamspaul@google.com>
2019-12-20 16:54:13 -0700

    Create wp-events-layout.php

---
+++ wp-events-layout.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Events Layout
+Plugin URI: https://github.com/paulprogrammer/wp-events-layout
+Description: Does some event layout tasks
+Version: 0.0.0
+Author: Me
+Author URI: https://github.com/paulprogrammer
+*/
