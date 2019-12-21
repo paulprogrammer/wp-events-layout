f681857e62b9228512fb48a159b25449033c3976
Paul Williams <paulprogrammer@gmail.com>
2019-12-20 17:23:29 -0700

    Update wp-events-layout.php

--- wp-events-layout.php
+++ wp-events-layout.php
@@ -4,6 +4,37 @@ Plugin Name: Events Layout
 Plugin URI: https://github.com/paulprogrammer/wp-events-layout
 Description: Does some event layout tasks
 Version: 0.0.0
-Author: Me
+Author: Paul Williams
 Author URI: https://github.com/paulprogrammer
 */
+
+function wpel_display_event($atts = []) {
+  // normalize attribute keys, lowercase
+  $atts = array_change_key_case((array)$atts, CASE_LOWER);
+
+  $query = new WP_Query( array(
+    'post_type' => 'event',
+    'order' => 'ASC',
+    'orderby' => 'date-time',
+  ) );
+  
+  if ( $query->have_posts() ) {
+    echo '<ul>';
+    while ( $query->have_posts() ) {
+      $query->the_post();
+      echo '<li>' . get_the_title() . '</li>';
+    }
+    echo '</ul>';
+  } else {
+      // no posts found
+  }
+  
+  // clean up after my query mess
+  wp_reset_postdata();
+}
+
+function wpel_shortcodes_init() {
+  add_shortcode( 'event_display', 'wpel_display_event');
+}
+ 
+add_action('init', 'wpel_shortcodes_init');
