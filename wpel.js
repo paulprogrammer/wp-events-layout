3de7c21dbb8d9f94f6b23a1d9440a85938033f9d
Paul Williams <paul@highaltitudearchery.com>
2019-12-22 12:39:13 -0700

    fix js

--- wpel.js
+++ wpel.js
@@ -1,8 +1,21 @@
 (function() {
+	if (window.NodeList && !NodeList.prototype.forEach) {
+	    NodeList.prototype.forEach = function (callback, thisArg) {
+	        thisArg = thisArg || window;
+	        for (var i = 0; i < this.length; i++) {
+	            callback.call(thisArg, this[i], i, this);
+	        }
+	    };
+	}
+
 	document.addEventListener("DOMContentLoaded", function(event) { 
-		document.findElementsByClassName(".event").click(function() {
-		  window.location = $(this).find("a").attr("href"); 
-		  return false;
+		let events = document.querySelectorAll(".event");
+		events.forEach(function(item) {
+			var self=item;
+			item.addEventListener('click', function(target) {
+				window.location = self.querySelector("a").getAttribute("href"); 
+		  	return false;
+			});
 		});
 	});
 })();
